f4a58e85738cb7a813e9b2ead40117bdcca65006
Anum Khalid <66093451+byanum@users.noreply.github.com>
2022-09-02 16:02:56 +0500

    map and list synchronization

--- Airbnb/src/screens/SearchMap/SearchResultMap.js
+++ Airbnb/src/screens/SearchMap/SearchResultMap.js
@@ -9,6 +9,14 @@ import feed from "../../../assets/data/feed";
 
 const SearchResultMap = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+  const map = useRef();
+  const viewConfig = useRef({ itemVisiblePercentThreshold: 70 });
+  const onViewChanged = useRef(({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      const selectedPlaceId = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlaceId.id);
+    }
+  });
   const width = useWindowDimensions().width;
   const myListRef = useRef(null);
 
@@ -19,11 +27,21 @@ const SearchResultMap = () => {
 
     const index = places.findIndex((place) => place.id === selectedPlaceId);
     myListRef.current.scrollToIndex({ index });
+
+    const selectedPlace = places[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.08,
+      longitudeDelta: 0.08,
+    };
+    map.current.animateToRegion(region);
   }, [selectedPlaceId]);
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={map}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -46,14 +64,16 @@ const SearchResultMap = () => {
 
       <View style={styles.carousel}>
         <FlatList
+          renderItem={({ item }) => <PostCarousel feeds={item} />}
+          data={feed}
           ref={myListRef}
           horizontal
           showsHorizontalScrollIndicator={false}
           snapToInterval={width - 60}
           snapToAlignment={"center"}
           decelerationRate={"fast"}
-          data={feed}
-          renderItem={({ item }) => <PostCarousel feeds={item} />}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
